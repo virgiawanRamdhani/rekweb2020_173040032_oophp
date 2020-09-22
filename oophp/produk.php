bf801b6bfe34667b368704e564cecf4ff4a0d8a4
Virgiawan Ramdhani <vramdhanii@mail.unpas.ac.id>
2020-09-22 23:54:33 +0700

    menambahkan produk3

--- oophp/produk.php
+++ oophp/produk.php
@@ -1,12 +1,23 @@
 <?php
 
-
 class Produk {
-    public $judul, 
-           $penulis,
-           $penerbit,
-           $harga;
+    public $judul = "judul", 
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
 }
 
-$produk1 = new Produk();
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "wkwkwk";
+// var_dump($produk2);
+
+$produk3 = new Produk ();
+$produk3->judul = "Naruto";
+$produk3->penerbit = "Mashasi kismono";
+$produk3->harga = 30000;
+var_dump($produk3);
