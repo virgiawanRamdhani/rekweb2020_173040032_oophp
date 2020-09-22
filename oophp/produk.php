40eacd7b267833be47777e476dc0229b4c410593
Virgiawan Ramdhani <vramdhanii@mail.unpas.ac.id>
2020-09-23 00:09:53 +0700

    tambah produk 4 video pak sandika #3

--- oophp/produk.php
+++ oophp/produk.php
@@ -5,6 +5,14 @@ class Produk {
            $penulis = "penulis",
            $penerbit = "penerbit",
            $harga = 0;
+
+ 
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    
+
 }
 
 // $produk1 = new Produk();
@@ -18,6 +26,19 @@ class Produk {
 
 $produk3 = new Produk ();
 $produk3->judul = "Naruto";
-$produk3->penerbit = "Mashasi kismono";
+$produk3->penulis = "Mashasi kismono";
+$produk3->penerbit = "Sony Computer";
 $produk3->harga = 30000;
-var_dump($produk3);
+
+$produk4 = new Produk ();
+$produk4->judul = "uncharted";
+$produk4->penulis = "neil";
+$produk4->penerbit = "nokia";
+$produk4->harga = 40000;
+
+echo "Komik : " . $produk3-> getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
+
+
+
