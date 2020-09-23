09f167de15fb1e9ed9843a76c2d59d7577471353
Virgiawan Ramdhani <vramdhanii@mail.unpas.ac.id>
2020-09-23 14:24:43 +0700

    menyelesaikan video #5 pak sandika

--- oophp/object-type.php
+++ oophp/object-type.php
@@ -18,13 +18,18 @@ class Produk {
         return "$this->penulis, $this->penerbit";
     }
 
-  
+}
 
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk -> judul} | {$produk -> getLabel()} (Rp. {$produk -> harga})";
+        return $str;
+    }
 }
 
 
-$produk1 = new Produk ("naruto","kishimoto nagato","shonen jump","30000");
-$produk2 = new Produk ("uncharted","sasuke nagato","sony computer","50000");
+$produk1 = new Produk ("Naruto","Kishimoto nagato","Shonen jump","30000");
+$produk2 = new Produk ("Uncharted","Sasuke nagato","Sony computer","50000");
 
 
 
@@ -32,6 +37,8 @@ $produk2 = new Produk ("uncharted","sasuke nagato","sony computer","50000");
 echo "Komik : " . $produk1-> getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
-
+echo "<br>";
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak ($produk1);
 
 
